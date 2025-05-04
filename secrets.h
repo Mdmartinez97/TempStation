d876095a45f549eed0735bb1d0f098dc613324cb
Marcos Martinez Borgiattino <101603101+Mdmartinez97@users.noreply.github.com>
2025-05-04 17:33:15 +0200

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -4,9 +4,9 @@ const char* WM_pass = "12345678";
 
 //******************* API's Key **************************
 //website: openweathermap.org/api
-const char* OpenWeatherMap_ApiKey = "79ec60437018d54a22094466dce934bc";
+const char* OpenWeatherMap_ApiKey = "";
 
 //website: aistudio.google.com/app/apikey
-const char* Gemini_ApiKey = "AIzaSyA2pDi2CZvlC9RqOE3M8j7mFIFIjXJfyA4";
+const char* Gemini_ApiKey = "";
 
 //********************* User Config **********************
